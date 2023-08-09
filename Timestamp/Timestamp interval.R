04e2a0a01aa6c6a1be7ed14c4deac667b62c4740
jingying0827 <jingyingwong827@gmail.com>
2023-08-09 22:00:33 +0800

    Initial commit

---
+++ Timestamp/Timestamp interval.R
@@ -0,0 +1,28 @@
+file007 = read.table("file007.txt",sep = ",",header = FALSE)
+
+# Set the file path
+file_path <- "file007.txt"  # Replace with the actual path to your file
+
+# Initialize a vector to store timestamps
+timestamps <- numeric()
+
+# Read the file line by line
+lines <- readLines(file_path)
+for (line in lines) {
+  if (startsWith(line, "*")) {
+    # Extract the timestamp after the asterisk
+    timestamp <- as.numeric(sub("^\\*(\\d+).*", "\\1", line))
+    timestamps <- c(timestamps, timestamp)
+  }
+}
+
+# Print the extracted timestamps
+print(timestamps)
+
+# Calculate the differences between consecutive timestamps
+timestamp_diffs <- diff(timestamps)
+
+# Create a plot
+plot(1:(length(timestamp_diffs)+1), timestamps, type = "o", 
+     xlab = "Timestamp Index", ylab = "Timestamp",
+     main = "Consistency of Timestamp Intervals")
