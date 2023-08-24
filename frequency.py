2496d02c1b8b6bd14f8cad7b4fa8c5f164e99127
jingying0827 <jingyingwong827@gmail.com>
2023-08-25 02:01:21 +0800

    Plot frequency graph by second, testing

--- frequency.py
+++ frequency.py
@@ -3,6 +3,8 @@ import datetime
 month = 2
 day = 15
 hour = 23
+minute = 1
+second = 1
 
 def read_file(filename):
     with open(filename,'r') as file:
@@ -18,23 +20,25 @@ def read_file(filename):
             day = timestamp.day
             hour = timestamp.hour
             minutes = timestamp.minute
+            seconds = timestamp.second
             
-            formatted_line = [year, month, day, hour, minutes] + col[1:]
+            formatted_line = [year, month, day, hour, minutes,seconds] + col[1:]
             formatted_data.append(formatted_line)
         #print(formatted_data[0:25])
     return formatted_data
 
+file_data  = read_file('./resources/file007_clean.txt')
+
 def get_month_data(month):
-    this_month_data = [['Day','Hour','Minutes','Index','X','Y','Z']]
-    data  = read_file('./resources/file007_clean.txt')
-    for line in data[1:]:
+    this_month_data = [['Day','Hour','Minutes','Seconds','Index','X','Y','Z']]
+    for line in file_data[1:]:
         if line[1] == month:
             this_month_data.append(line[2:])
     #print(this_month_data[0:25])
     return this_month_data
 
-def get_day_data(day):
-    this_day_data = [['Hour','Minutes','Index','X','Y','Z']]
+def get_day_data(month, day):
+    this_day_data = [['Hour','Minutes','Seconds','Index','X','Y','Z']]
     data  = get_month_data(month)
     for line in data[1:]:
         if line[0] == day:
@@ -42,15 +46,36 @@ def get_day_data(day):
     #print(this_day_data[0:25])
     return this_day_data
 
-def get_hour_data(hour):
-    this_hour_data = [['Minutes','Index','X','Y','Z']]
-    data  = get_day_data(day)
+def get_hour_data(month, day, hour):
+    this_hour_data = [['Minutes','Seconds','Index','X','Y','Z']]
+    data  = get_day_data(month, day)
     #print(data[0:10])
     for line in data[1:]:
         if line[0] == hour:
             this_hour_data.append(line[1:])
     #print(this_hour_data)
     return this_hour_data
+
+def get_minute_data(month, day, hour, minute):
+    this_minute_data = [['Seconds','Index','X','Y','Z']]
+    data  = get_hour_data(month, day, hour)
+    #print(data[0:10])
+    for line in data[1:]:
+        if line[0] == minute:
+            this_minute_data.append(line[1:])
+    #print(this_minute_data)
+    return this_minute_data
+
+def get_second_data(month, day, hour, minute, second):
+    this_second_data = [['Index','X','Y','Z']]
+    data  = get_minute_data(month, day, hour, minute)
+    #print(data[0:10])
+    for line in data[1:]:
+        if line[0] == second:
+            this_second_data.append(line[1:])
+    #print(this_second_data)
+    return this_second_data
+
 
 def frequency_x_per_hour(hr):
     data = get_hour_data(hr)
@@ -67,9 +92,36 @@ def frequency_x_per_hour(hr):
     #print(hour,':00', "frequency of movement",frequency)
     return frequency
 
+def frequency_x_per_second(month, day, hour, minute, second):
+    data = get_second_data(month, day, hour, minute, second)
+    previous_x = None
+    frequency = 0
+    for line in data[1:]:
+        x = line[1]
+        if previous_x is None:
+            previous_x = int(x)
+        elif abs(int(x) - int(previous_x)) >= 10:
+            frequency += 1
+        previous_x = x
+    #print(data[0:10])
+    #print(previous_x)
+    print(month, day, hour, minute, second)
+    #print(hour,':00', "frequency of movement",frequency)
+    return frequency
+
+def frequency_x_per_hour_by_second(month, day):
+    x_frequency = [["Time","Frequency of movement in x-axis"]]
+    for min in range (60):
+        for sc in range(60):
+            x_frequency.append([str(month)+'/'+str(day)+' '+str(hour)+':'+str(min)+':'+str(sc),frequency_x_per_second(month, day, hour, min, sc)])
+    print(x_frequency)
+    return x_frequency
+frequency_x_per_hour_by_second(month, day)
+
+
 def frequency_x_per_day():
     x_frequency = [["Hour","Frequency of movement in x-axis"]]
-    for hr in range(25):
+    for hr in range(24):
         x_frequency.append([hr,frequency_x_per_hour(hr)])
     #print(x_frequency)
     return x_frequency
