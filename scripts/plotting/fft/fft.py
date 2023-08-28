350a5225f426e80d68ba5655084ed81077659e23
Hasley Deniel Walker <23031585@student.uwa.edu.au>
2023-08-28 18:46:38 +0800

    updates on fft plot

--- scripts/plotting/fft/fft.py
+++ scripts/plotting/fft/fft.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+from scipy.fftpack import fft,fftfreq
 
 with open('./resources/file007_clean.txt', 'r') as file:
     lines = file.readlines()
@@ -18,8 +19,14 @@ for line in lines:
 
 # Convert timestamps to datetime objects
 datetimes = [datetime.utcfromtimestamp(ts) for ts in times]
+# Calculate the time difference between consecutive datetimes
+time_diff = (datetimes[-1] - datetimes[0]).total_seconds() / (len(datetimes) - 1)
 
-sample_rate = len(datetimes)
+print(time_diff)
+# Calculate the sample rate as the inverse of the time difference
+sample_rate = 1 / time_diff
+sample = len(datetimes)
+
 def plot_fft(time,axis,sample_rate,direction):
     # This returns the fourier transform coeficients as complex numbers
     transformed_axis = np.fft.fft(axis)
@@ -28,7 +35,7 @@ def plot_fft(time,axis,sample_rate,direction):
     freqs_magnitude = np.abs(transformed_axis)
 
     # Create frequency x-axis that will span up to sample_rate
-    freq_axis = np.linspace(0, sample_rate, len(freqs_magnitude))
+    freq_axis = np.linspace(0, sample//2, num = len(freqs_magnitude))
 
     # Plot frequency domain
     plt.plot(freq_axis, freqs_magnitude)
@@ -38,6 +45,43 @@ def plot_fft(time,axis,sample_rate,direction):
     plt.xlim(0, 100)
     plt.show()
 
-plot_fft(time,xs,sample_rate,"x")
-plot_fft(time,ys,sample_rate,"y")
-plot_fft(time,zs,sample_rate,"z")
+# plot_fft(datetimes,xs,sample_rate,"x")
+# plot_fft(datetimes,ys,sample_rate,"y")
+# plot_fft(datetimes,zs,sample_rate,"z")
+
+
+def plot_fft2(axis):
+    axis -= np.mean(axis)
+    fft = np.fft.fft(axis)
+    fftfreq = np.fft.fftfreq(len(axis))
+    plt.ylabel("Amplitude")
+    plt.xlabel("Frequency [Hz]")
+    plt.plot(fftfreq[fftfreq>=0], np.abs(fft)[fftfreq>=0])
+    plt.show()
+
+# plot_fft2(xs)
+# plot_fft2(ys)
+# plot_fft2(zs)
+
+#source: https://www.alphabold.com/fourier-transform-in-python-vibration-analysis/
+def fft_plot3(axis):
+    sample_rate = 25
+    total_second = (datetimes[-1] - datetimes[0]).total_seconds()
+    N = sample_rate*total_second
+    frequency = np.linspace(0.0, (sample_rate/2), int (N/2))
+
+    freq_data = fft(axis)
+    y = 2/N * np.abs (freq_data [0: int(N/2)])
+    #y = y - np.mean(y)
+
+    plt.plot(frequency[frequency>=0], y[frequency>=0])
+    plt.ylim(0,2)
+    plt.title('Frequency domain Signal')
+    plt.xlabel('Frequency in Hz')
+    plt.ylabel("Amplitude")
+    plt.show()
+
+fft_plot3(xs)
+fft_plot3(ys)
+fft_plot3(zs)
+# check: https://stackoverflow.com/questions/69356006/fast-fourier-transform-of-subset-of-vibration-dataset
