3c9a9bac7237fa11c233ce15af659cfd3c6bc5f8
Mat <mat.cook@bigpond.com>
2023-10-03 09:11:58 +0800

    Added a button to load data before displaying the other gui components

--- scripts/interface/gui.py
+++ scripts/interface/gui.py
@@ -2,26 +2,22 @@ import os
 import sys
 import tkinter as tk
 from tkinter import ttk  # For the Combobox widget
+from tkinter import filedialog
 import importlib
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 PLOTTING_DIR = os.path.join(os.path.dirname(__file__), "..", "plotting")
+load_data_button = None
 
+def load_data():
+    global load_data_button
+    directory = filedialog.askdirectory()  # Open directory selection dialog
+    if directory:
+        # If a directory is selected, hide the load_data_button and display the rest of the GUI components
+        load_data_button.pack_forget()
+        display_gui_components()
 
-def run_gui():
-    global root
-    root = tk.Tk()
-
-    # Maximizing the window based on the platform
-    if sys.platform == "win32":
-        root.state('zoomed')
-    elif sys.platform == "linux" or sys.platform == "linux2":
-        root.attributes('-zoomed', True)
-    elif sys.platform == "darwin":
-        w, h = root.winfo_screenwidth() * 0.8, root.winfo_screenheight() * 0.8
-        x, y = (root.winfo_screenwidth() - w) // 2, (root.winfo_screenheight() - h) // 2
-        root.geometry(f"{int(w)}x{int(h)}+{int(x)}+{int(y)}")
-
+def display_gui_components():
     # Frame for top buttons
     top_frame = tk.Frame(root)
     top_frame.pack(side=tk.TOP, fill=tk.X)
@@ -29,14 +25,6 @@ def run_gui():
     # Create a Combobox
     combobox = ttk.Combobox(top_frame)
     combobox.pack(side=tk.LEFT, pady=20, padx=20)
-
-    # Button to display graph
-    btn = tk.Button(top_frame, text="Display Graph", command=lambda: display_graph(combobox))
-    btn.pack(side=tk.RIGHT, padx=5, pady=10)  # This ensures the button is on the right side of the top frame, but to the left of the Print Data button
-
-    # Button to print data
-    print_button = tk.Button(top_frame, text="Print Data", command=print_data)
-    print_button.pack(side=tk.RIGHT, padx=5, pady=10) 
 
     # Populate Combobox with Python scripts from all subdirectories in the plotting folder
     scripts = []
@@ -48,10 +36,13 @@ def run_gui():
 
     combobox['values'] = scripts
 
-    root.mainloop()
+    # Button to display graph
+    btn = tk.Button(top_frame, text="Display Graph", command=lambda: display_graph(combobox))
+    btn.pack(side=tk.RIGHT, padx=5, pady=10)
 
-
-current_canvas = None
+    # Button to print data
+    print_button = tk.Button(top_frame, text="Print Data", command=print_data)
+    print_button.pack(side=tk.RIGHT, padx=5, pady=10)
 
 def print_data():
     print('test')
@@ -81,6 +72,31 @@ def display_graph(combobox):
 
     # Update current_canvas to hold a reference to the current canvas
     current_canvas = canvas
+
+def run_gui():
+    global load_data_button
+    global root
+    root = tk.Tk()
+
+    # Maximizing the window based on the platform
+    if sys.platform == "win32":
+        root.state('zoomed')
+    elif sys.platform == "linux" or sys.platform == "linux2":
+        root.attributes('-zoomed', True)
+    elif sys.platform == "darwin":
+        w, h = root.winfo_screenwidth() * 0.8, root.winfo_screenheight() * 0.8
+        x, y = (root.winfo_screenwidth() - w) // 2, (root.winfo_screenheight() - h) // 2
+        root.geometry(f"{int(w)}x{int(h)}+{int(x)}+{int(y)}")
+
+    # Initially, only display the "Load Data" button in the middle
+    load_data_button = tk.Button(root, text="Load Data", command=load_data)
+    load_data_button.pack(pady=20)
+
+    root.mainloop()
+
+
+current_canvas = None
+
 
 if __name__ == "__main__":
     run_gui()
