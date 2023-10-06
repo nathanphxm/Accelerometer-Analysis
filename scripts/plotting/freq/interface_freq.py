eb40d3db42e305decb8d4c3b0e313c708874b0b8
jingying0827 <jingyingwong827@gmail.com>
2023-10-06 21:53:40 +0800

    implement gui code for freq plotting

--- scripts/plotting/freq/interface_freq.py
+++ scripts/plotting/freq/interface_freq.py
@@ -8,11 +8,6 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime
 from datetime import timedelta
-import csv
-
-# Define thresholds for low, moderate, and high activity
-low_activity_threshold = 500
-high_activity_threshold = 1000
 
 # Define frequency detecting threshold
 freq_detect_threshold = 10
@@ -52,24 +47,6 @@ def get_freq(data):
             minute_list = [line[1:]]
 
     return frequencies
-
-# function to classify different activity level
-def classify_activity(frequency, low_threshold, high_threshold):
-    if frequency <= low_threshold:
-        return "Low Activity"
-    elif frequency <= high_threshold:
-        return "Moderate Activity"
-    else:
-        return "High Activity"
-
-# function to provide different colour to each activity level
-def color_by_activity(activity):
-    if activity == "Low Activity":
-        return 'blue'
-    elif activity == "Moderate Activity":
-        return 'green'
-    else:
-        return 'red'
     
 # function to plot graph of processed frequency data
 def plot_graph(data):
@@ -78,52 +55,20 @@ def plot_graph(data):
     
     # Extract x-axis (time) and y-axis (frequency) data
     time = [entry[0] for entry in freq_data[1:]]  # Skipping the header row
-    cumulative_freq = 0
-    frequency = []
-    for entry in freq_data[1:]:
-        cumulative_freq += entry[1]
-        frequency.append(cumulative_freq)
+    frequency = [entry[1] for entry in freq_data[1:]]
 
     # Create a line plot for cumulative frequency
     fig = plt.figure(figsize=(12, 6), facecolor='white')
-    plt.plot(time, frequency, marker=None, color='blue', label='Cumulative Frequency')
-
-    # Create a list to store the rows for the CSV
-    csv_rows = [['Datetime', 'Frequency per Minute', 'Activity Level']]
-
-    # Add lower-opacity squares to differentiate activity levels
-    for i in range(len(time)):
-        all_freq = freq_data[1:]
-        freq = all_freq[i][1]
-        act_level = classify_activity(freq, low_activity_threshold, high_activity_threshold)
-        color = color_by_activity(act_level)
-        plt.fill_between([time[i]], cumulative_freq, color=color, alpha=0.3)
-        row = [time[i], freq, act_level]
-        csv_rows.append(row)
-
-    # Specify the CSV file path
-    csv_file_path = 'output.csv'
-
-    # Write the data to the CSV file
-    with open(csv_file_path, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerows(csv_rows)
-
-    # Display a message indicating where the CSV file was saved
-    print(f'CSV file saved at: {csv_file_path}')
+    plt.plot(time, frequency, marker= None, color='blue', label='Frequency')
 
     plt.xlabel('Time (GMT+8)')
-    plt.ylabel('Cumulative Frequency of Movement')
-    plt.title(f'Cumulative Frequency of Movement by Minute')
+    plt.ylabel('Frequency of Movement')
+    plt.title('Frequency of Movement by Minute')
 
     # Format the x-axis to display time at 1-hour intervals
     ax = plt.gca()
     ax.xaxis.set_major_locator(mdates.HourLocator(interval=1))
     ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
-
-    # Customise legend
-    handles = [plt.Rectangle((0, 0), 1, 1, color=color_by_activity(act)) for act in ["Low Activity", "Moderate Activity", "High Activity"]]
-    plt.legend(handles, ["Low Activity", "Moderate Activity", "High Activity"])
 
     plt.xticks(rotation=90)
     plt.grid(False)
